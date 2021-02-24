7b5cfbad6fcb02c261b330cca49ee6c5bafbffa3
aleidagonzalez <aleida1983@hotmail.com>
2021-02-24 15:00:08 +0000

    cambios

--- generador.js
+++ generador.js
@@ -1,26 +1,29 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
 
-function sampleFromArray(array){
-    return array[Math.floor(Math.random() *  array.length)];
-}
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [ "before the class","right on time","when I finished", "during my lunch", "while I was praying"];
+ 
+  
+window.onload = function() {
+  //write your code here
+  console.log("Hello Rigo from the console!");
+  let whoWord = who[Math.floor(Math.random() * who.length)];
+  let actionWord = action[Math.floor(Math.random() * action.length)];
+  let whatWord = what[Math.floor(Math.random() * what.length)];
 
-function excuseGenerator(){
-    let who_elements = ['The dog', 'His turtle', 'My Grandma', 'My bird'];
-    let action_elements = ['ate', 'peed', 'crushed', 'broke'];
-    let what_elements = ['my homework', 'the keys', 'the car'];
-    let when_elements = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+  let whenWord = when[Math.floor(Math.random() * when.length)];
 
-    let who =  sampleFromArray(who_elements);
-    let action =  sampleFromArray(action_elements);
-    let what =  sampleFromArray(what_elements);
-    let when =  sampleFromArray( when_elements);
-    
-    let excuse = who + ' ' + action + ' ' + what + ' ' + when + '.'
+  let excuseMessage =
+    whoWord + " " + actionWord + " " + whatWord + " " + whenWord + ".";
 
-    return excuse;
-}
-    
+  let excuseContainer = document.querySelector("#excuse"); // Equivalente : document.getElementById("excuse");
+  excuseContainer.innerText = excuseMessage;
+};
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById("excuse").innerHTML = excuseGenerator();
-});
